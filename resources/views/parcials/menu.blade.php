39df2142410016de6be8256a97d0207146fa6180
Nuno Silva <silvanuno@gmail.com>
2021-05-28 22:25:07 +0000

    Configuração de webpack

--- resources/views/parcials/menu.blade.php
+++ resources/views/parcials/menu.blade.php
@@ -3,7 +3,7 @@
     <a class="navbar-brand">Carrinho de Compras</a>
     <form class="d-flex">
       <input class="form-control me-2" type="search" placeholder="Procurar" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit"><i class="fa fa-search"></i></button>
+      <button class="btn btn-outline-success" type="submit"><i class="bi bi-search"></i></button>
     </form>
   </div>
 </nav>
