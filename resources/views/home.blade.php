1815844121322e55cb7ad1e772a769b7efbf7d58
Nuno Silva <silvanuno@gmail.com>
2021-05-28 21:45:58 +0000

    commit inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('conteudo')
+  <h1 class="text-white">Carrinho de Compras</h1>
+@endsection
---
+++ resources/views/parcials/menu.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-dark bg-dark shadow-lg p-3 mb-5 rounded">
+  <div class="container-fluid">
+    <a class="navbar-brand">Carrinho de Compras</a>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Procurar" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit"><i class="fa fa-search"></i></button>
+    </form>
+  </div>
+</nav>
